8464174e4e55ccec92c84be0da68fb50c4147f2f
umib <msumib@nigge.rs>
2020-12-09 11:08:07 -0300

    first

---
+++ app/src/main/java/umib/dots/httpbooks/Category.kt
@@ -0,0 +1,10 @@
+package umib.dots.httpbooks
+
+import com.google.gson.annotations.SerializedName
+
+data class Category (
+    @SerializedName("categoria")
+    var name: String = "",
+    @SerializedName("livros")
+    var books: List<Book> = emptyList()
+)
---
+++ app/src/main/java/umib/dots/httpbooks/MainActivity.kt
@@ -0,0 +1,24 @@
+package umib.dots.httpbooks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_books_list.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        this.listView.setOnItemClickListener { parent, view, position, id ->
+            val book = parent.getItemAtPosition(position) as? Book
+            if (book != null) {
+                Toast.makeText(
+                    this,
+                    "${book.title}, \n ${book.author}, \n ${book.year} ",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+}
